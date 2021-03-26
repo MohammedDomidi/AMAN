491adfa4a542fa1a52346110c2497fc2c5b0f769
MohammedDomidi <ironmanmoedom@gmail.com>
2021-03-26 22:51:13 +0300

    nablus work2

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tab3',
@@ -9,7 +11,22 @@ export class Tab3Page implements OnInit {
   Type:String;
 
 
-  constructor() {}
+  Data: Observable<any>;
+  temp = 0 ;
+  rate = 0 ;
+
+
+  constructor( public httpClient: HttpClient) {
+    setInterval(()=>{
+      this.Data = this.httpClient.get('http://192.168.43.37:5000/skin');
+    this.Data
+    .subscribe(data => {
+     this.temp =data ;
+
+    })
+    },5000)
+
+  }
 
   ngOnInit(){
     this.Type="temp";
