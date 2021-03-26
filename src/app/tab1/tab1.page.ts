5e2672fcec2878585e879a5a6c8b4da72f40ac3d
MohammedDomidi <ironmanmoedom@gmail.com>
2021-03-26 19:46:19 +0300

    nablus work

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  Data: Observable<any>;
+  temp = 0 ;
+  hum = 0 ;
+  Type: String ;
 
+  constructor( public httpClient: HttpClient) {
+    setInterval(()=>{
+      this.Data = this.httpClient.get('http://192.168.43.37:5000/DHT');
+    this.Data
+    .subscribe(data => {
+     this.temp = ~~data.temp ;
+     this.hum = ~~data.hum;
+    })
+    },20000)
+
+  }
+  ngOnInit(){
+    this.Type = "temp"
+  }
+
+  segmentChanged(ev:any){
+    console.log('Segment changed', ev);
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  Type:String;
+
 
   constructor() {}
 
+  ngOnInit(){
+    this.Type="temp";
+  }
+  segmentChanged(event:any){
+
+  }
 }
